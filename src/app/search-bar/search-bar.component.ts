07ddaec3ddcfd82ccde417ea1144f8fa99961f5b
Karthik Mohanasundaram <connect2kms@outlook.com>
2021-12-03 11:29:40 -0500

    initial checlin

---
+++ src/app/search-bar/search-bar.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-search-bar',
+  templateUrl: './search-bar.component.html',
+  styleUrls: ['./search-bar.component.css'],
+})
+export class SearchBarComponent implements OnInit {
+  term = '';
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  onInput(event: Event) {
+    const value = (event.target as HTMLInputElement).value;
+    this.term = value;
+  }
+}
